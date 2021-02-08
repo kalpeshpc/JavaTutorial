9b3c5392f061cb75f4cd7be8a5277ea5325200ef
kalpesh <kalpeshpchaudhari@gmail.com>
2021-02-08 17:19:13 +0530

    formatting code

--- ProjectQ/src/com/java/array/BiggerNumberinArray.java
+++ ProjectQ/src/com/java/array/BiggerNumberinArray.java
@@ -4,46 +4,39 @@ public class BiggerNumberinArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int []arr= {2,4,6,7,5,21,553,43,32};
- System.out.println(biggerNumberIntoArray(arr));
- System.out.println(addallValues(arr));
- System.out.println(averageOfArrayValues(arr));
+		int[] arr = { 2, 4, 6, 7, 5, 21, 553, 43, 32 };
+		System.out.println(biggerNumberIntoArray(arr));
+		System.out.println(addallValues(arr));
+		System.out.println(averageOfArrayValues(arr));
 
 	}
-	
-	public static int biggerNumberIntoArray(int[] a)
-	{
-		int bigNo=a[0];
-		for(int k:a)
-		{
-			if(bigNo<k)
-			{
-				bigNo=k; //replacing the bigger NUmber with k .
+
+	public static int biggerNumberIntoArray(int[] a) {
+		int bigNo = a[0];
+		for (int k : a) {
+			if (bigNo < k) {
+				bigNo = k; // replacing the bigger NUmber with k .
 			}
 		}
 		return bigNo;
 	}
-	
-	public static int addallValues(int []arr)
-	{
-		int total=0;
-		for(int k:arr)
-		{
-			total=total+k;
+
+	public static int addallValues(int[] arr) {
+		int total = 0;
+		for (int k : arr) {
+			total = total + k;
 		}
-		
+
 		return total;
 	}
-	
-	public static int averageOfArrayValues(int []arr)
-	{
-		int average=0;
-		for(int k:arr)
-		{
-			average=average+k;
+
+	public static int averageOfArrayValues(int[] arr) {
+		int average = 0;
+		for (int k : arr) {
+			average = average + k;
 		}
-		
-		return average/arr.length;
+
+		return average / arr.length;
 	}
 
 }
--- ProjectQ/src/com/java/string/ReverseWord.java
+++ ProjectQ/src/com/java/string/ReverseWord.java
@@ -1,44 +1,29 @@
 package com.java.string;
 
-
-
 public class ReverseWord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String content="my Name is Kalpesh";
-String n=reverseWord(content,0);
-System.out.println(n);
+		String content = "my Name is Kalpesh";
+		String n = reverseWord(content, 0);
+		System.out.println(n);
 	}
-	
-	
-	public static String reverseWord(String str,int n)
-	{
+
+	public static String reverseWord(String str, int n) {
 		String result = "";
-		String st="";
-//		StringTokenizer st1=new StringTokenizer(str," ");
-//		while(st1.hasMoreTokens())
-//		{
-//			st1.l
-//		}
-		String s[]=str.split(" ");
-		
-		if(n>=0) {
-		for(int i=s[n].length()-1;i>=0;i--)
-		{
-			st+=s[n].charAt(i);
+		String st = "";
+		String s[] = str.split(" ");
+
+		if (n >= 0) {
+			for (int i = s[n].length() - 1; i >= 0; i--) {
+				st += s[n].charAt(i);
+			}
+			s[n] = st;
 		}
-		s[n]=st;
+		for (String s1 : s) {
+			result += s1 + " ";
 		}
-		for(String s1:s)
-		{
-			result+=s1+" ";
-		}
-	//	result=Arrays.toString(s);  //convert array into String but its having comma seperated string.
-		
-		
 		return result;
 	}
-	
 
 }
